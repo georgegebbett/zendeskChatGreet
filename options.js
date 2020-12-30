11be6db924e6cf152e95dce2d9d522dfa5c9b01e
George Gebbett <george.gebbett1@gmail.com>
2020-12-30 14:42:50 +0000

    troubleshooting options added to options page

--- options.js
+++ options.js
@@ -6,21 +6,46 @@ function save_options() {
             console.log("Greeting updated");
             document.getElementById("successMsg").hidden = false;
             document.getElementById("errorMsg").hidden = true;
+            document.getElementById("deleteMsg").hidden = true;
+
         })
     } else {
         document.getElementById("errorMsg").hidden = false;
         document.getElementById("successMsg").hidden = true;
+        document.getElementById("deleteMsg").hidden = true;
+
         console.log("No.")
     }
 
 }
 
 function restore_options() {
-    chrome.storage.sync.get({greeting}, function (items){
+    chrome.storage.sync.get({greeting: "Hi there [name]"}, function (items){
         document.getElementById("greeting").value = items.greeting;
     })
 }
 
+function delete_options() {
+    chrome.storage.sync.clear(function (){
+        document.getElementById("deleteMsg").hidden = false;
+    })
+}
+
+function hideShowOptions() {
+    if (document.getElementById("devOp").checked === true){
+        document.getElementById("remove").hidden = false;
+        document.getElementById("retrieve").hidden = false;
+    } else {
+        document.getElementById("remove").hidden = true;
+        document.getElementById("retrieve").hidden = true;
+    }
+}
+
+
+
 
 document.addEventListener('DOMContentLoaded', restore_options);
 document.getElementById("save").addEventListener('click', save_options);
+document.getElementById("remove").addEventListener('click', delete_options);
+document.getElementById("retrieve").addEventListener('click', restore_options);
+document.getElementById("devOp").addEventListener('click', hideShowOptions);
