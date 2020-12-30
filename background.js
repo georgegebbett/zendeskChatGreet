419d450225110ef2405063aaa9b4152b3490fd1b
George Gebbett <george.gebbett1@gmail.com>
2020-12-30 15:38:28 +0000

    readme improved
    improvement to regex for normal customer names so first name is always returned regardless of number of names in name

--- background.js
+++ background.js
@@ -24,7 +24,7 @@ chrome.commands.onCommand.addListener(function(command) {
     		console.log(/^.+\((\w+).*\)/.exec(promptName))
     		promptName = /^.+\((\w+).*\)/.exec(promptName)[1];
     	} else if (/^\w+\s\w+$/.test(promptName)) {
-    		promptName = /^(\w+)\s\w+$/.exec(promptName)[1];
+    		promptName = /^(\w+)[\w ]+$/.exec(promptName)[1];
     	}
 		var fullGreet = greeting.replace("[name]", " ".concat(promptName.substring(0, 1).toUpperCase(), promptName.substring(1)));
 		chrome.tabs.executeScript(tabs[0].id, {code:"document.getElementsByClassName(\"meshim_dashboard_components_chatPanel_ChatTextAreaList chat_input valid\")[0].value = \"".concat(fullGreet).concat("\"")})
